6446d0dc18a47592ebc92cdf7d393b548de912db
KatNezabydka <katorif@ukr.net>
2018-10-04 21:35:14 +0300

    laravel-echo chat

--- resources/assets/js/components/PrivateChatComponent.vue
+++ resources/assets/js/components/PrivateChatComponent.vue
@@ -5,7 +5,9 @@
             <div class="col-sm-12">
                 <textarea class="form-control" rows="10" readonly="">{{ messages.join('\n') }}</textarea>
                 <hr>
-                <input type="text" class="form-control" v-model="textMessage" @keyup.enter="sendMessage">
+                <input type="text" class="form-control" v-model="textMessage" @keyup.enter="sendMessage"
+                       @keydown="actionUser">
+                <span v-if="isActive">{{ isActive.name }} набирает сообщение...</span>
             </div>
         </div>
     </div>
@@ -14,22 +16,42 @@
 
 <script>
     export default {
-        props: ['room'],
+        props: ['room', 'user'],
         data(){
            return {
                messages: [],
-               textMessage: ''
+               textMessage: '',
+               isActive: false,
+               //чтобы не накладывалось набирает сообщение от нескольких пользователей
+               typingTimer: false
            }
+        },
+        computed: {
+            channel() {
+                return  window.Echo.private('room.' + this.room);
+            }
         },
         //момент монтирования компонента
         mounted() {
             //отвечает за прослушивание канала со стороны пользователя
             //channel - какой канал
-            //lesten - какое событие прослушиваем
+            //listen - какое событие прослушиваем
             //при срабатывании используем стрелочную функцию
-            window.Echo.private('room.' + this.room)
-                    .listen('PrivateChat', ({data}) => {
+           this.channel.listen('PrivateChat', ({data}) => {
                         this.messages.push(data.body)
+                        //когда сообщение пришло, убираем надпись
+                        this.isActive = false;
+            })
+            .listenForWhisper('typing', (e) => {
+                //будет получать массив данных, в данном случае имя пользователя   this.channel.whisper('typing', {name: this.user.name});
+                this.isActive = e;
+
+            //если таймер истино отменяем выполнение и присваиваем значение таймера этому свойству
+            if(this.typingTimer) clearTimeout(this.typingTimer);
+                //задаем время счерез сколько убирать сообщение
+            this.typingTimer = setTimeout(() => {
+                    this.isActive = false;
+                }, 2000);
             });
         },
         methods: {
@@ -39,6 +61,12 @@
                 this.messages.push(this.textMessage);
 
                 this.textMessage = '';
+            },
+            actionUser() {
+                //whisper - транслирует событие в канал, что пользователь набирает сообщение
+                this.channel.whisper('typing', {
+                    name: this.user.name
+                });
             }
         }
     }
